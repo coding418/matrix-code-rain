39dad93fabd0d567cdf066ae73a5c0ad6a7f56e6
coding418 <patrickeveritt@gmail.com>
2021-05-02 00:53:03 +0100

    Added new previews

---
+++ docs/scripts.js
@@ -0,0 +1,16 @@
+vids = [
+	"matrix-code-rain-v0.3-downpour-mode.mp4",
+	"matrix-code-rain-v0.3-operator-mode.mp4",
+	"matrix-code-rain-v0.3-pixel-mode.mp4",
+]
+
+titles = [
+	"Downpour Mode v0.3",
+	"Operator Mode v0.3",
+	"Webcam/Pixel Mode v0.3",
+]
+
+function change_vid(idx){
+	vsrc = document.getElementById("vidsrc");
+	vsrc.src = "https://github.com/coding418/matrix-code-rain/blob/main/vid/" + vids[idx] + "?raw=true";
+}
